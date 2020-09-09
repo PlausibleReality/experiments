4e60b1734f9812e2ecbfb4cbc917b0a59bc31ec4
Bobby <bobby@plausiblereality.com>
2020-09-09 18:39:21 +0000

    better comments/layout for caddy file, link to unity test page

--- client/pages/unity.tsx
+++ client/pages/unity.tsx
@@ -1,5 +1,6 @@
 export default function UnityTest() {
     return <div>
-        Up next - loading a unity scene
+        First pass - the unity test is hosted on a separate page:<br/>
+        <a href="/assets/unity_assets/build0/WebGLBuild/index.html">Unity Test</a>
     </div>
 }
